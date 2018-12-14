1bb1695324a44e2c8be4d08bf89dfd0a74267a0f
bewing03 <ben.ewing114@gmail.com>
2018-12-14 11:01:57 -0500

    HTTPS

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ fs.readFile('secret_keys.txt', 'utf8', function (err, contents) {
 });
 
 
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+// var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+var redirect_uri = `https://spotify-visualization.herokuapp.com/callback`
 
 /**
  * Generates a random string containing numbers and letters
